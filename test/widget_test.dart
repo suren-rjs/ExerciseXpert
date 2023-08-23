b0cd11b0463f2383b0794f778f48ee02ac00d690
Surendiran <suren.rjs@gmail.com>
2023-08-23 23:21:27 +0530

    Pagination Added

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,10 +8,8 @@ import 'package:lottie/lottie.dart';
 void main() {
   testWidgets('SplashLoadingScreen displays Lottie animation',
       (WidgetTester tester) async {
-    // await tester.pumpWidget(const MaterialApp(home: SplashLoadingScreen()));
-    //
-    // expect(find.byType(Lottie), findsOneWidget);
-      const currentPage = 1;
-      print("Page from ${(currentPage-1) * 10} to ${currentPage * 10}");
+    await tester.pumpWidget(const MaterialApp(home: SplashLoadingScreen()));
+
+    expect(find.byType(Lottie), findsOneWidget);
   });
 }
